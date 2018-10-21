d119839ada7bb10f3dc095de6256be3453c710ba
Grayson Gullion <xinflux@gmail.com>
2018-10-21 19:21:39 -0400

    make github see my changes

--- src/main/java/basemod/patches/com/megacrit/cardcrawl/rooms/AbstractRoom/PreRenderHook.java
+++ src/main/java/basemod/patches/com/megacrit/cardcrawl/rooms/AbstractRoom/PreRenderHook.java
@@ -7,6 +7,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
+//this is before the player and monsters are rendered but after the room scene is rendered
 @SpirePatch(clz= AbstractRoom.class, method = "render")
 public class PreRenderHook {
 	@SpirePrefixPatch
