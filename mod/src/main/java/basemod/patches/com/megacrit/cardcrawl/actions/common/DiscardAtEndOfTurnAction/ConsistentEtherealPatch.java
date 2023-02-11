30e733401d8e4d291ff9117cbc9b672a156f5809
Alchyr <isithalchyr@gmail.com>
2023-02-11 00:47:58 -0800

    Seeded But Not Rng

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/actions/common/DiscardAtEndOfTurnAction/ConsistentEtherealPatch.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/actions/common/DiscardAtEndOfTurnAction/ConsistentEtherealPatch.java
@@ -3,7 +3,9 @@ package basemod.patches.com.megacrit.cardcrawl.actions.common.DiscardAtEndOfTurn
 import basemod.BaseMod;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInstrumentPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.DiscardAtEndOfTurnAction;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
@@ -23,12 +25,17 @@ public class ConsistentEtherealPatch {
             public void edit(MethodCall m) throws CannotCompileException {
                 if (m.getMethodName().equals("shuffle") && m.getClassName().equals(Collections.class.getName())) {
                     m.replace("if (" + BaseMod.class.getName() + ".fixesEnabled) {" +
-                            Collections.class.getName() + ".shuffle($1, new java.util.Random(" + AbstractDungeon.class.getName() + ".shuffleRng.randomLong()));" +
+                            Collections.class.getName() + ".shuffle($1, new java.util.Random(" + ConsistentEtherealPatch.class.getName() + ".arbitrarySomewhatSeedRelatedNumber()));" +
                             "} else {" +
                             "$proceed($$);" +
                             "}");
                 }
             }
         };
+    }
+
+    private static final long FLOOR_OFFSET = 55;
+    public static long arbitrarySomewhatSeedRelatedNumber() {
+        return (Settings.seed == null ? 0 : Settings.seed) + AbstractDungeon.floorNum * FLOOR_OFFSET + GameActionManager.turn;
     }
 }
