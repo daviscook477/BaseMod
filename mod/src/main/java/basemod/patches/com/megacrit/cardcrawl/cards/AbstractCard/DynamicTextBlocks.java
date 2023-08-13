a32a86a3d91e0853c34305bee96ff981fde3dd02
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-13 17:08:00 -0300

    Fix import

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/DynamicTextBlocks.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/DynamicTextBlocks.java
@@ -6,7 +6,6 @@ import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.screens.ExhaustPileViewScreen;
