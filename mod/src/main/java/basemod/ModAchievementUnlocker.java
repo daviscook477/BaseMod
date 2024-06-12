a5265a6212086b8f2cb3935660dab75db155cf7b
Cany0udance <blakkrum@aol.com>
2024-06-11 22:24:50 -0400

    Removed comment and publisher integration

--- mod/src/main/java/basemod/ModAchievementUnlocker.java
+++ mod/src/main/java/basemod/ModAchievementUnlocker.java
@@ -1,6 +1,5 @@
 package basemod;
 
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 
 import java.util.List;
@@ -11,9 +10,7 @@ import static com.megacrit.cardcrawl.unlock.UnlockTracker.achievementPref;
 public class ModAchievementUnlocker {
     public static void unlockAchievement(String key) {
         if (!Settings.isShowBuild && Settings.isStandardRun()) {
-            CardCrawlGame.publisherIntegration.unlockAchievement(key);
             if (!achievementPref.getBoolean(key, false)) {
-                BaseMod.logger.info("Attempting to unlock achievement with key: " + key);
                 achievementPref.putBoolean(key, true);
                 achievementPref.flush();
 
